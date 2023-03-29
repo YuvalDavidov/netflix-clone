13aaaa33351d729fc3c94740859ad6998d3f6ad0
Yuval <yuval838@gmail.com>
2023-03-29 14:27:55 +0300

    add Main cmp and Requset api from TMDB

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
+import { Route, Routes } from "react-router-dom";
 import NavBar from "./cmps/NavBar";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <NavBar/>
+<Routes>
+<Route path="/" element={<Home/>}/>
 
+</Routes>
     </>
   );
 }
